8148d0c3004ec31781f0c59908d1c4e56e6d946b
Nathan Jessurun <ntjessu@gmail.com>
2023-12-10 23:59:25 -0600

    improve packaging script.
    Don't hard-code file list and package dir, get them from toml file instead

--- package.py
+++ package.py
@@ -8,13 +8,11 @@ import tomli
 
 FilePath = t.Union[str, Path]
 
-PACKAGE_PATHS = ["src", "lib.typ", "LICENSE", "README.md"]
-
-here = Path(__file__).resolve().parent
-
 
 def create_package(
+    source_folder: FilePath,
     typst_packages_folder: FilePath,
+    package_paths: t.List[str | FilePath],
     version: str,
     package_name: str,
     namespace="preview",
@@ -28,18 +26,18 @@ def create_package(
     upload_folder = Path(typst_packages_folder) / namespace / package_name / version
     if upload_folder.exists() and not exist_ok:
         raise FileExistsError(f"{upload_folder} already exists")
-    upload_folder.mkdir(parents=True, exist_ok=True)
+    elif upload_folder.exists():
+        shutil.rmtree(upload_folder)
+    upload_folder.mkdir(parents=True)
 
-    toml_text = here.joinpath("typst.toml").read_text()
-    toml_text = toml_text.replace("{{version}}", version)
-    upload_folder.joinpath("typst.toml").write_text(toml_text)
-    for path in map(Path, PACKAGE_PATHS):
+    src = Path(source_folder)
+    for path in map(Path, package_paths):
         if path.is_dir():
             shutil.copytree(
-                here.joinpath(path), upload_folder.joinpath(path), dirs_exist_ok=True
+                src.joinpath(path), upload_folder.joinpath(path), dirs_exist_ok=True
             )
         else:
-            shutil.copy(here.joinpath(path), upload_folder.joinpath(path))
+            shutil.copy(src.joinpath(path), upload_folder.joinpath(path))
     return upload_folder
 
 
@@ -47,6 +45,7 @@ if "__main__" == __name__:
     import argparse
     import os
 
+    here = Path(__file__).resolve().parent
     default_packages_folder = os.environ.get("typst_packages_folder", None)
 
     parser = argparse.ArgumentParser()
@@ -56,13 +55,18 @@ if "__main__" == __name__:
     parser.add_argument("--typst-packages-folder", default=default_packages_folder)
     args = parser.parse_args()
 
-    with open(args.toml, "rb") as ifile:
+    toml_file = Path(args.toml).resolve()
+    with open(toml_file, "rb") as ifile:
         toml_text = tomli.load(ifile)  # type: ignore
     version = toml_text["package"]["version"]
     package_name = toml_text["package"]["name"]
+    package_paths = toml_text["tool"]["packager"]["paths"]
+    package_paths.append(toml_file.name)
 
     folder = create_package(
+        Path(args.toml).resolve().parent,
         args.typst_packages_folder,
+        package_paths,
         version,
         package_name,
         namespace=args.namespace,
